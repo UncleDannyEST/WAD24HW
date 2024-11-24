f08dd358740f13d6758ab491549f83bcd252aa0f
askar <askar.neumann@gmail.com>
2024-11-25 00:33:44 +0200

    Date showing

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -4,6 +4,7 @@
     <p>{{ post.text }}</p>
     <img v-if="post.image_link" :src="post.image_link" alt="Post Image" />
     <p>Likes: {{ post.likes }}</p>
+    <p class="post-date">Posted on: {{ formattedDate }}</p> <!-- Display formatted date -->
     <img 
       src="thumbsup.jpg" 
       alt="Like"
@@ -16,6 +17,13 @@
 <script>
 export default {
   props: ['post'], // Receive the post object as a prop
+  computed: {
+    // Format the date to a readable format
+    formattedDate() {
+      const date = new Date(this.post.creation_date);
+      return date.toLocaleDateString(); // This will format the date based on the user's locale
+    },
+  },
   methods: {
     likePost() {
       this.$store.dispatch('likePost', this.post.id);
@@ -37,6 +45,12 @@ export default {
 .post img {
   max-width: 100%;
   border-radius: 8px;
+  margin-top: 10px;
+}
+
+.post-date {
+  font-size: 0.9em;
+  color: #777;
   margin-top: 10px;
 }
 </style>
